7e955858349ee373fa32bc9d21c9373fd7b1e0b0
ismail-cankaya <ismayilcankaya2017@gmail.com>
2026-07-29 00:22:22 +0300

    feat: create PublishInvitationAction class for invitation management

---
+++ app/Actions/Invitation/PublishInvitationAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Actions\Invitation;
+
+class PublishInvitationAction
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
